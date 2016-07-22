f5c1f83613d46bf885eb7539fab959ef17f0a43e
PrzemyslawMadej <przemyslawmd@gmail.com>
2016-07-22 22:31:26 +0200

    File for base class

---
+++ Compression/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Compression
+{
+    class Node
+    {
+        public Node() { }
+
+        public Node( Boolean isLeaf )
+        {
+            this.isLeaf = isLeaf;
+        }
+
+        public byte ByteValue
+        {
+            get { return byteValue; }
+            set { byteValue = value; }
+        }
+
+        public Boolean Leaf
+        {
+            get { return isLeaf; }
+        }
+
+        protected Boolean isLeaf;
+        protected byte byteValue;
+    }
+}
