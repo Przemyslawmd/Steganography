f71d5c644f8525874c33a0c7f06936fcdeb7d08e
Przemyslaw Madej <p.madej@samsung.com>
2016-12-21 17:02:23 +0100

    Test for data alignment : Encryption class

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Cryptography;
 
 namespace Tests
@@ -8,9 +9,27 @@ namespace Tests
     public class TestsCryptography
     {
         [TestMethod]
-        public void Test1()
-        {           
-        
+        public void TestDataAlignment()
+        {                                
+            PrivateObject obj = new PrivateObject( new Encryption());
+
+            List<byte> data = new List<byte>( new byte[5] );  
+            obj.Invoke( "AlignData", data );
+
+            Assert.AreEqual(data.Count, 16);
+            Assert.AreEqual( data[data.Count - 1], 11 );
+
+            data = new List<byte>( new byte[22] );
+            obj.Invoke( "AlignData", data );
+
+            Assert.AreEqual( data.Count, 32 );
+            Assert.AreEqual( data[data.Count - 1], 10 );
+
+            data = new List<byte>( new byte[16] );
+            obj.Invoke( "AlignData", data );
+
+            Assert.AreEqual( data.Count, 32 );
+            Assert.AreEqual( data[data.Count - 1], 16 );
         }
     }
 }
