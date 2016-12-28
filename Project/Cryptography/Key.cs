2a1c7480741e2f5ce35819cc568167f4f476bc2c
PrzemyslawMadej <przemyslawmd@gmail.com>
2016-12-28 22:49:50 +0100

    AES : Test for key expansion added

--- Project/Cryptography/Key.cs
+++ Project/Cryptography/Key.cs
@@ -7,13 +7,24 @@ namespace Cryptography
     class Key
     {
         public static byte[] CreateKey( String password )
+        {            
+            byte[] basicKey = new Rfc2898DeriveBytes( password, salt, iterations ).GetBytes( keyBytes );
+            return ExpandKey( basicKey );
+        }
+
+
+        private static byte[] ExpandKey( byte[] key )
         {
-            return new Rfc2898DeriveBytes( password, salt, iterations ).GetBytes( keyBytes );
+            byte[] expandedKey = new byte[176];
+
+            Array.Copy( key, 0, expandedKey, 0, keyBytes );
+
+            return expandedKey;
         }
 
         static readonly byte[] salt = new byte[] { 4, 32, 3, 112, 34, 11, 45, 26, 4, 34 };
         static readonly int iterations = 200;
-        static readonly int keyBytes = 32;
+        static readonly int keyBytes = 16;
     }
 }
 
