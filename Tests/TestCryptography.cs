417669008b72673fdf1b47d033a21d38208044d3
Przemyslawmd <przemyslawmd@gmail.com>
2017-03-06 23:04:22 +0100

    Second test for data block encryption

--- Tests/TestCryptography.cs
+++ Tests/TestCryptography.cs
@@ -19,6 +19,8 @@ namespace Tests
             PrivateType typeKey = new PrivateType( typeof( Key ) );
             PrivateObject typeEncryption = new PrivateObject( new Encryption() );
 
+            // First test
+
             initialKey = new byte[16]       { 0x54, 0x68, 0x61, 0x74,
                                               0x73, 0x20, 0x6D, 0x79,
                                               0x20, 0x4B, 0x75, 0x6E,
@@ -38,7 +40,30 @@ namespace Tests
 
             roundKeys = (byte[][])typeKey.InvokeStatic( "ExpandKey", initialKey );            
             typeEncryption.Invoke( "EncryptBlockData", blockToEncrypt, roundKeys );
-            CollectionAssert.AreEqual( blockToEncrypt, blockExpected );         
+            CollectionAssert.AreEqual( blockToEncrypt, blockExpected );
+
+
+            // Second test
+
+            initialKey = new byte[16]       { 0x2B, 0x7E, 0x15, 0x16,
+                                              0x28, 0xAE, 0xD2, 0xA6,
+                                              0xAB, 0xF7, 0x15, 0x88,
+                                              0x09, 0xCF, 0x4F, 0x3C };
+
+            blockToEncrypt = new byte[4, 4] {{ 0x32, 0x88, 0x31, 0xE0 },
+                                             { 0x43, 0x5A, 0x31, 0x37 },
+                                             { 0xF6, 0x30, 0x98, 0x07 },
+                                             { 0xA8, 0x8D, 0xA2, 0x34 }};
+
+
+            blockExpected = new byte[4, 4]  {{ 0x39, 0x02, 0xDC, 0x19 },
+                                             { 0x25, 0xDC, 0x11, 0x6A },
+                                             { 0x84, 0x09, 0x85, 0x0B },
+                                             { 0x1D, 0xFB, 0x97, 0x32 }};
+                                    
+            roundKeys = (byte[][])typeKey.InvokeStatic( "ExpandKey", initialKey );
+            typeEncryption.Invoke( "EncryptBlockData", blockToEncrypt, roundKeys );
+            CollectionAssert.AreEqual( blockToEncrypt, blockExpected );
         }
 
         /************************************************************************************************************/
