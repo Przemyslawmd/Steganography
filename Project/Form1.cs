0038a03a9033f5908350241f1b0114e11d151072
PrzemyslawMadej <przemyslawmd@gmail.com>
2017-07-04 00:23:12 +0200

    Form 'About' in a separate file

--- Project/Form1.cs
+++ Project/Form1.cs
@@ -251,28 +251,7 @@ namespace Stegan
 
         private void ShowAbout(object sender, EventArgs e)
         {
-            Form Form = new Form();
-            Form.Width = 470;
-            Form.Height = 260;
-            Form.BackColor = Color.White;
-            Form.MaximizeBox = false;
-            Form.MinimizeBox = false;
-            Form.ShowIcon = false;
-            Form.FormBorderStyle = FormBorderStyle.FixedSingle;
-            
-            WebBrowser browser = new WebBrowser();
-            browser.Dock = DockStyle.Fill;
-            browser.DocumentText = "<html><body style='font-size:11px; font-family: Arial; line-height:150%; margin-top:15px; margin-left:15px;'>" + 
-                "<p style='font-weight:bold; font-size:12px; letter-spacing:2px;'>Steganography application</p>" + 
-                "<pre>Version 1.3.1</pre>" +
-                "<pre>Author:           Przemyslaw Madej, Warsaw 2017 </pre>" + 
-                "<pre>Email:            przemyslawmd@gmail.com</pre>" +
-                "<pre>Home page:        http://www.przemeknet.pl/steganEn.aspx</pre></body></html>"; 
-            Form.Controls.Add(browser);
-
-            Form.Disposed += ( object obj, EventArgs eventA ) => { menuStripOne.Enabled = true; };
-            menuStripOne.Enabled = false;
-            Form.Show();
+            FormAbout formAbout = new FormAbout( menuStripOne );
         }
 
         /********************************************************************************************/
