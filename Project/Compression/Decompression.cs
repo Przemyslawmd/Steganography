5b2b544f8d31cf96eae3849cb19d30737734c1c9
PrzemyslawMadej <przemyslawmd@gmail.com>
2017-07-07 20:36:11 +0200

    Creating tree for decompression moved into HuffmanTree class

--- Project/Compression/Decompression.cs
+++ Project/Compression/Decompression.cs
@@ -17,8 +17,8 @@ namespace Stegan
             dataCount = BitConverter.ToInt32( source.ToArray(), dataIndex + 1 );
 
             dataIndex += 4;
-            BuildTree( codes );
-            return Decode( source );
+            Node root = new HuffmanTree().BuildTreeDecompression( codes );
+            return Decode( source, root );
         }
 
         /**********************************************************************************/
@@ -77,48 +77,11 @@ namespace Stegan
             return codes;
         }
 
-        /********************************************************************************/
-        /* BUILD TREE *******************************************************************/
-
-        private void BuildTree( Dictionary<byte, List<char>> codes )
-        {
-            root = new Node( 0 );
-            Node node;
-
-            foreach ( KeyValuePair<byte, List<char>> code in codes )
-            {
-                node = root;
-
-                // Traverse chars in code in exception of first and last char - each code begins with char '1'
-                foreach ( char token in code.Value.Skip( 1 ).Take( code.Value.Count - 2 ))
-                {
-                    if ( token == '0' )
-                    {
-                        if ( node.Left == null )
-                            node.Left = new Node( 0 );
-                        node = node.Left;
-                    }
-                    else
-                    {
-                        if ( node.Right == null )
-                            node.Right = new Node( 0 );
-                        node = node.Right;
-                    }
-                }
-
-                // Check last char in code - add leaf
-                if ( code.Value.Last() == '0' )
-                    node.Left = new Node( code.Key );
-                else
-                    node.Right = new Node( code.Key );
-             }
-        }
-
         /*********************************************************************************/
         /* DECODE ************************************************************************/
         // Decompress data - change codes into byte values
 
-        private List<byte> Decode( List<byte> source )
+        private List<byte> Decode( List<byte> source, Node root )
         {                        
             List<byte> decompressedData = new List<byte>();
             byte[] mask = { 128, 64, 32, 16, 8, 4, 2, 1 };
@@ -162,7 +125,6 @@ namespace Stegan
         /**************************************************************************************/
 
         int dataCount;                      // count of bytes to be decompressed
-        private Node root;
         private int dataIndex;              // index in sourceData
     }
 }
--- Project/Compression/HuffmanTree.cs
+++ Project/Compression/HuffmanTree.cs
@@ -7,7 +7,7 @@ namespace Stegan
     class HuffmanTree
     {
         /*****************************************************************************************/
-        /* BUILD TREE ****************************************************************************/
+        /* BUILD TREE FOR COMPRESSION ************************************************************/
         // Public method for buiding Huffman tree
         // Parameter is stream of bytes to be compressed
         // Return a root of Huffman tree
@@ -18,6 +18,44 @@ namespace Stegan
             nodes = nodes.OrderBy( x => x.Count ).ToList();
             BuildTree( nodes );
             return nodes[0];
+        }
+
+        /*****************************************************************************************/
+        /* BUILD TREE FOR DECOMPRESSION **********************************************************/
+
+        public Node BuildTreeDecompression( Dictionary<byte, List<char>> codes )
+        {
+            Node root = new Node( 0 );
+            Node node;
+
+            foreach ( KeyValuePair<byte, List<char>> code in codes )
+            {
+                node = root;
+
+                // Traverse chars in code in exception of first and last char - each code begins with char '1'
+                foreach ( char token in code.Value.Skip( 1 ).Take( code.Value.Count - 2 ) )
+                {
+                    if ( token == '0' )
+                    {
+                        if ( node.Left == null )
+                            node.Left = new Node( 0 );
+                        node = node.Left;
+                    }
+                    else
+                    {
+                        if ( node.Right == null )
+                            node.Right = new Node( 0 );
+                        node = node.Right;
+                    }
+                }
+
+                // Check last char in code - add leaf
+                if ( code.Value.Last() == '0' )
+                    node.Left = new Node( code.Key );
+                else
+                    node.Right = new Node( code.Key );
+            }
+            return root;
         }
 
         /*****************************************************************************************/
