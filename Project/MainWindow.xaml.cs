879cf5a6761f335122fedfc16e8c904382ae71c9
Przemyslawmd <przemyslawmd@gmail.com>
2018-05-31 17:29:09 +0200

    Action to open a graphic file handled

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
 using System.Windows;
+using Microsoft.Win32;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -15,14 +19,33 @@ using System.Windows.Shapes;
 
 namespace Steganography
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /**************************************************************************************/
+        /**************************************************************************************/
+
+        private void OpenGraphicFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = LoadGraphic.Header.ToString();
+            open.Filter = "Graphical file|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+
+            if ( open.ShowDialog() == false )
+            {
+                MessageBox.Show(" Loading graphic file failed" );
+                return;
+            }
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri( open.FileName );
+            bitmap.EndInit();
+            imageControl.Source = bitmap;
         }
     }
 }
