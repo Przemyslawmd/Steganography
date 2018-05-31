dfb6e89eee35b75bed19456fba7b12d1b25fdcf1
Przemyslawmd <przemyslawmd@gmail.com>
2018-05-31 23:04:11 +0200

    Style changes

--- Project/Covering/BaseCover.cs
+++ Project/Covering/BaseCover.cs
@@ -3,12 +3,12 @@ namespace Stegan
 {
     abstract class BaseCover
     {
-        protected int bitNumber;            // Number of bit in a byte 
-        protected int byteCount;            // Count of bytes in a stream of data to be covered / uncovered                        
+        protected int bitNumber;                        // Number of bit in a byte
+        protected int byteCount;                        // Count of bytes in a stream of data to be covered / uncovered
         protected byte byteValue;           
-        protected const byte MaskOne = 1;
-        protected const int CompressPixel = 6;         // Number of a pixel which stores an information about compresion      
-        protected const int DataSizePixel = 6;         // Count of pixels intented to store an information about a size of data                         
+        protected const byte MaskOne = 0x01;
+        protected const int CompressPixel = 6;          // Number of a pixel which stores an information about compresion
+        protected const int DataSizePixel = 6;          // Count of pixels intented to store a size of data
         protected const int LastBit = 7;
     }
 }
