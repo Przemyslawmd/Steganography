c9e86dbd2e37a4b2010536fc2365f12d510139bc
Przemyslawmd <przemyslawmd@gmail.com>
2018-06-03 00:53:23 +0200

    Style changes

--- Project/Compression/HuffmanCodes.cs
+++ Project/Compression/HuffmanCodes.cs
@@ -7,42 +7,42 @@ namespace Stegan
 {
     class HuffmanCodes
     {
-        /********************************************************************************/
-        /* CREATE CODES DICTIONARY ******************************************************/        
-
         public Dictionary<byte, List<char>> CreateCodesDictionary( NodeCompress root )
         {
-            codes = new Dictionary<byte, List<char>>();
-            code = new List<char>();
+            codes = new Dictionary< byte, List< char >>();
+            code = new List< char >();
             GenerateCodes( root, '1' );
             return codes;
         }
 
-        /********************************************************************************/
-        /* GENERATE CODES ***************************************************************/
-        // Recursive method that traverses tree and generates codes 
+        /**************************************************************************************/
+        /**************************************************************************************/
 
         private void GenerateCodes( Node node, char token )
         {
             code.Add( token );
 
             if ( node.Left != null )
+            {
                 GenerateCodes( node.Left, '0' );
+            }
             if ( node.Right != null )
+            {
                 GenerateCodes( node.Right, '1' );
+            }
             else
-                codes.Add( node.ByteValue, new List<char>( code ));
+            {
+                codes.Add( node.ByteValue, new List< char >( code ) );
+            }
 
             code.RemoveAt( code.Count - 1 );
         }
 
-        /***********************************************************************************/
-        /***********************************************************************************/
+        /**************************************************************************************/
+        /**************************************************************************************/
 
-        // In some extreme cases, code may be very long, this is the reason of using List<Char> instead of, 
-        // for example, Long type 
-        private List<char> code;
-
-        private Dictionary<byte, List<char>> codes;
+        private List< char > code;
+        private Dictionary< byte, List< char >> codes;
     }
 }
+
