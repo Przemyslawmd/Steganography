a0f409c471a8cba1d2ab5fb04e7aded013596994
Przemyslawmd <przemyslawmd@gmail.com>
2018-06-09 11:08:10 +0200

    Handled actions for loading and removing data from file

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.IO;
 using System.Windows;
 using Microsoft.Win32;
+using System.Collections.Generic;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -22,12 +23,11 @@ namespace Steganography
         private void ActionOpenGraphic(object sender, RoutedEventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();
-            open.Title = LoadGraphic.Header.ToString();
+            open.Title = MenuLoadGraphic.Header.ToString();
             open.Filter = "Graphical file|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
 
             if ( open.ShowDialog() == false )
             {
-                MessageBox.Show(" Loading graphic file failed" );
                 return;
             }
 
@@ -38,14 +38,14 @@ namespace Steganography
 
             if ( bitmapImage.Width > imageBorder.ActualWidth || bitmapImage.Height > imageBorder.ActualHeight )
             {
-                imageControl.Stretch = Stretch.Uniform;
+                ControlImage.Stretch = Stretch.Uniform;
             }
             else
             {
-                imageControl.Stretch = Stretch.None;
+                ControlImage.Stretch = Stretch.None;
             }
 
-            imageControl.Source = bitmapImage;
+            ControlImage.Source = bitmapImage;
             ChangeControlState( true, MenuSaveGraphic, MenuRemoveGraphic );
             MenuSaveGraphic.IsEnabled = true;
         }
@@ -59,12 +59,7 @@ namespace Steganography
             saveDialog.Filter = "PNG|*.png|BMP|*.bmp";
             saveDialog.Title = "Save Graphic File";
 
-            if ( saveDialog.ShowDialog() == false )
-            {
-                return;
-            }
-
-            if ( saveDialog.FileName == "" )
+            if ( saveDialog.ShowDialog() == false || saveDialog.FileName == "" )
             {
                 return;
             }
@@ -79,7 +74,7 @@ namespace Steganography
                 encoder = new BmpBitmapEncoder();
             }
 
-            encoder.Frames.Add( BitmapFrame.Create( (BitmapSource) imageControl.Source ));
+            encoder.Frames.Add( BitmapFrame.Create( (BitmapSource) ControlImage.Source ));
             MemoryStream outStream = new MemoryStream();
             encoder.Save( outStream );
             Bitmap bitmap = new Bitmap( outStream );
@@ -91,8 +86,45 @@ namespace Steganography
 
         private void ActionRemoveGraphic( object sender, RoutedEventArgs e )
         {
-            imageControl.Source = null;
+            ControlImage.Source = null;
             ChangeControlState( false, MenuSaveGraphic, MenuRemoveGraphic );
+        }
+
+        /**************************************************************************************/
+        /**************************************************************************************/
+
+        private void ActionLoadFile( object sender, RoutedEventArgs e )
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = MenuLoadFile.HeaderStringFormat;
+
+            if ( open.ShowDialog() == false )
+            {
+                return;
+            }
+
+            FileStream fileStream = new FileStream( open.FileName, FileMode.Open, FileAccess.Read );
+            BinaryReader binary = new BinaryReader( fileStream );
+            FileInfo fileInfo = new FileInfo( open.FileName );
+            dataBuffer = new List< byte >( binary.ReadBytes( (int) fileInfo.Length ));
+
+            ControlData.Text = "A file was loaded: " + fileInfo.Name + " : " + (dataBuffer.Count / 1024) + "kB";
+            ChangeControlState( true, MenuRemoveData );
+            ChangeControlState( false, MenuLoadFile );
+
+            if ( ControlImage.Source != null )
+            {
+                ChangeControlState( true, MenuCoverData );
+            }
+        }
+
+        /**************************************************************************************/
+        /**************************************************************************************/
+
+        private void ActionRemoveData( object sender, RoutedEventArgs e )
+        {
+            dataBuffer.Clear();
+            ControlData.Text = "";
         }
 
         /**************************************************************************************/
@@ -105,6 +137,11 @@ namespace Steganography
                 menu.IsEnabled = state;
             }
         }
+
+        /**************************************************************************************/
+        /**************************************************************************************/
+
+        private List< byte > dataBuffer;
     }
 }
 
