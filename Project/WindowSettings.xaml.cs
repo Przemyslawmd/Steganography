40890f768cc721300cb9b48a34c789da11b562a2
Przemyslawmd <przemyslawmd@gmail.com>
2018-06-12 22:35:19 +0200

    WindowSettings added

---
+++ Project/WindowSettings.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Windows;
+
+namespace Steganography
+{
+    public partial class WindowSettings : Window
+    {
+        public WindowSettings()
+        {
+            InitializeComponent();
+            IsCompression.IsChecked = Settings.Encryption;
+            IsEncryption.IsChecked = Settings.Compression;
+            Password.Text = Settings.Password;
+        }
+
+        /**************************************************************************************/
+        /**************************************************************************************/
+
+        private void ActionAccept( object sender, RoutedEventArgs e )
+        {
+            Settings.Encryption = (bool) IsCompression.IsChecked;
+            Settings.Compression = (bool) IsEncryption.IsChecked;
+            Settings.Password = Password.Text;
+            Close();
+        }
+    }
+}
