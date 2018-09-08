70f8f0cdb11328694ee607b56daf03a184b71e77
Przemyslawmd <przemyslawmd@gmail.com>
2018-09-08 10:15:02 +0100

    No two separate stream indices while getting codes dictionary from a stream to be decompression

--- Project/Compression/Decompression.cs
+++ Project/Compression/Decompression.cs
@@ -9,9 +9,9 @@ namespace SteganographyCompression
     {
         public List< byte > Decompress( List< byte > source )
         {
-            Dictionary< byte, List< bool >> codesDictionary = GetCodesFromSource( source  );
-            originalSize = BitConverter.ToInt32( source.ToArray(), dataIndex );
-            dataIndex += 4;
+            Dictionary< byte, List< bool >> codesDictionary = GetCodesDictionaryFromStream( source  );
+            originalSize = BitConverter.ToInt32( source.ToArray(), streamIndex );
+            streamIndex += 4;
 
             Node root = new HuffmanTree().BuildTreeDecompression( codesDictionary );
             return Decode( source, root );
@@ -20,30 +20,33 @@ namespace SteganographyCompression
         /**************************************************************************************/
         /**************************************************************************************/
 
-        private Dictionary< byte, List< bool >> GetCodesFromSource( List<byte> data )
+        private Dictionary< byte, List< bool >> GetCodesDictionaryFromStream( List< byte > stream )
         {
-            // Get size of codes stream and count of codes and move dataIndex
-            int codesDictionarySize = BitConverter.ToInt32( data.GetRange(0, 4).ToArray(), 0 );
-            int codesCount = ( data[4] == 0 ) ? 256 : data[4];
-            dataIndex = 5;
+            int codesDictionarySize = BitConverter.ToInt32( stream.GetRange( 0, 4 ).ToArray(), 0 );
+            int codesCount = ( stream[4] == 0 ) ? 256 : stream[4];
+            streamIndex = 5;
 
-            Dictionary< byte, List< bool >> codesDictionary = new Dictionary< byte, List< bool >>();
-
-            // Stream index indicates the beginning of bits sequence that represent codes
-            int codesIndex = dataIndex + codesCount * 2;
-            int bitIndex = 1;
-            List< bool > code = new List< bool >();
+            Dictionary < byte, byte > codesList = new Dictionary< byte, byte >();
 
             for ( int i = 0; i < codesCount; i++ )
             {
-                GetCodeFromStream( code, data, data[dataIndex + 1], ref codesIndex, ref bitIndex );
-                codesDictionary.Add( data[dataIndex], new List< bool >( code ));
-                code.Clear();
-                dataIndex += 2;
+                codesList.Add( stream[streamIndex], stream[streamIndex+1] );
+                streamIndex += 2;
             }
 
-            // Move data index at the place where codes end
-            dataIndex = codesDictionarySize;
+            Dictionary< byte, List< bool >> codesDictionary = new Dictionary< byte, List< bool >>();
+
+            int bitIndex = 1;
+            List< bool > code = new List< bool >();
+
+            foreach ( KeyValuePair< byte, byte > codeSymbol in codesList )
+            {
+                GetCodeFromStream( code, stream, codeSymbol.Value, ref streamIndex, ref bitIndex );
+                codesDictionary.Add( codeSymbol.Key, new List< bool >( code ) );
+                code.Clear();
+            }
+
+            streamIndex = codesDictionarySize;
             return codesDictionary;
         }
 
@@ -72,7 +75,7 @@ namespace SteganographyCompression
             List< byte > decompressedData = new List< byte >();
             Node node = null;
 
-            foreach ( byte byteValue in source.Skip( dataIndex ))
+            foreach ( byte byteValue in source.Skip( streamIndex ))
             {
                 for ( int bitNumber = 1; bitNumber <= 8; bitNumber++ )
                 {
@@ -111,7 +114,7 @@ namespace SteganographyCompression
         /**************************************************************************************/
 
         int originalSize;                   // Size of data before compression
-        private int dataIndex;              // It is global index in a source data to be decompressed
+        private int streamIndex;              // It is global index in a source data to be decompressed
     }
 }
 
