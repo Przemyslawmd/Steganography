5326e45803998d693cc9357d847691add17353d9
PrzemyslawMadej <przemyslawmd@gmail.com>
2019-11-13 01:09:27 +0100

    Separate class with constant values added

---
+++ Project/ConstValues.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Steganography
+{
+    class ConstValues
+    {
+        public static readonly int BitsInByte = 8; 
+        public static readonly int CompressionPixelNumber = 8;
+        public static readonly int CountOfPixelsForDataSize = 8;
+        public static readonly int FirstRow = 0;
+        public static readonly int SecondRow = 1;
+        public static readonly byte MaskOne = 0x01;
+        public static readonly byte[] CoverMark = { 0x6B, 0xA9 };
+    }
+}
+
